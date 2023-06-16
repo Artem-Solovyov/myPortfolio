0304d836dfb3c8b20d2a45b39373523d98f26a1e
Artem-Solovyov <108827233+Artem-Solovyov@users.noreply.github.com>
2023-06-16 16:28:12 +0300

    Add files via upload

---
+++ gallery/showcase3d/js/common.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const showSlider = new Swiper(".showcase__carousel", {
+    loop: true,
+    slidesPerView: 3,
+    speed: 1800,
+    centeredSlides: true,
+    navigation: {
+      nextEl: ".showcase-navigation-next",
+      prevEl: ".showcase-navigation-prev",
+    },
+  });
+  document.querySelector("video").playbackRate = 2;
+});
